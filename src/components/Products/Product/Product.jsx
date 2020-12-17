3c396a554c9c4a3a7373bb82c76d8f5f91de9204
naglec13 <naglec13@gmail.com>
2020-12-17 19:44:49 +0300

    -#2- add product component

---
+++ src/components/Products/Product/Product.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
+  IconButton,
+} from "@material-ui/core";
+import classes from "*.module.css";
+import { AddShoppingCart } from "@material-ui/icons";
+
+const Product = ({ product }) => {
+  return (
+    <Card className={classes.root}>
+      <CardMedia className={classes.media} image="" title={product.title} />
+      <CardContent>
+        <div className={classes.content}>
+          <Typography variant="h5">{product.name}</Typography>
+          <Typography variant="h5">{product.price}</Typography>
+        </div>
+        <Typography variant="h2" color="textSecondary">
+          {product.description}
+        </Typography>
+      </CardContent>
+      <CardActions className={classes.cardActions} disableSpacing>
+        <IconButton aria-label="Add to Cart">
+          <AddShoppingCart />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default Product;
