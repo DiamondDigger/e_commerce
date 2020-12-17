57a99201124f54f4167f51ec96a970563575216a
naglec13 <naglec13@gmail.com>
2020-12-17 19:53:04 +0300

    -#2- set props

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -13,10 +13,12 @@ import { AddShoppingCart } from "@material-ui/icons";
 const Product = ({ product }) => {
   return (
     <Card className={classes.root}>
-      <CardMedia className={classes.media} image="" title={product.title} />
+      <CardMedia className={classes.media} image="" title={product.name} />
       <CardContent>
         <div className={classes.content}>
-          <Typography variant="h5">{product.name}</Typography>
+          <Typography variant="h5" gutterBottom>
+            {product.name}
+          </Typography>
           <Typography variant="h5">{product.price}</Typography>
         </div>
         <Typography variant="h2" color="textSecondary">
