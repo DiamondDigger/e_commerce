d02bb13827489cba6b79feef46ccf5046de9ed99
naglec13 <naglec13@gmail.com>
2020-12-18 00:40:42 +0300

    -#3- add jsx for the component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Badge,
+  MenuItem,
+  Menu,
+  Typography,
+  Icon,
+} from "@material-ui/core";
+import { ShoppingCart } from "@material-ui/icons";
+
+import logo from "../../assets/logo.png";
+
+const Navbar = () => {
+  return (
+    <>
+      <AppBar position="fixed" className={classes.appBar} color="inherit">
+        <Toolbar>
+          <Typography variant="h6" classesName={classes.title} color="inherit">
+            <img
+              src={logo}
+              alt="Commers.js"
+              height="25px"
+              className={classes.image}
+            />
+            Commers.js
+          </Typography>
+          <div className={classes.grow} />
+          <div className={classes.button}>
+            <IconButton aria-label="Show cart items" color="inherit">
+              <Badge badgeContent={2}>
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
+
+export default Navbar;
