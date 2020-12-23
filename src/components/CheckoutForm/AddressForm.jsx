67705038b0f8a4391dfa79f43665a6ee9e9ad0c1
naglec13 <naglec13@gmail.com>
2020-12-23 20:10:49 +0300

    -#12- add select section with states

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -12,6 +12,13 @@ import { useForm, FormProvider } from "react-hook-form";
 import FormInput from "./CustomTextField";
 
 const AddressForm = () => {
+  const [shippingCountries, setshippingCountries] = useState([]);
+  const [shippingCountry, setshippingCountry] = useState("");
+  const [shippingSubdivisions, setshippingSubdivisions] = useState([]);
+  const [shippingSubdivision, setshippingSubdivision] = useState("");
+  const [shippingOptions, setshippingOptions] = useState([]);
+  const [shippingOption, setshippingOption] = useState("");
+
   const methods = useForm();
 
   return (
@@ -28,6 +35,26 @@ const AddressForm = () => {
             <FormInput required name="email" label="Email" />
             <FormInput required name="city" label="City" />
             <FormInput required name="zip" label="ZIP / Postal code" />
+            <Grid item xs={12} sm={6}>
+              <InputLabel>Shipping Contry</InputLabel>
+              <Select value={} fullWidth onChange={}>
+                <MenuItem key={} value={}>
+                  Select Me
+                </MenuItem>
+              </Select>
+              <InputLabel>Shipping Subdivision</InputLabel>
+              <Select value={} fullWidth onChange={}>
+                <MenuItem key={} value={}>
+                  Select Me
+                </MenuItem>
+              </Select>
+              <InputLabel>Shipping Options</InputLabel>
+              <Select value={} fullWidth onChange={}>
+                <MenuItem key={} value={}>
+                  Select Me
+                </MenuItem>
+              </Select>
+            </Grid>
           </Grid>
         </form>
       </FormProvider>
