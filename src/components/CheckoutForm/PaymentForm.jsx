c721d41162254f179cbe4e91c3b5ad9be609b811
naglec13 <naglec13@gmail.com>
2020-12-27 01:21:46 +0300

    -#17- add base frame for the payment section with stripe

--- src/components/CheckoutForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -3,15 +3,31 @@ import { Typography, Button, Divider } from "@material-ui/core";
 import {
   Elements,
   CardElement,
-  ElementConsumer,
+  ElementsConsumer,
 } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import Review from "./Review";
+
+const stripePromise = loadStripe("stripePublicApiKey");
 
 const PaymentForm = ({ checkoutToken }) => {
   return (
     <>
       <Review checkoutToken={checkoutToken} />
+      <Typography variant="h6" gutterBottom>
+        Payment method
+      </Typography>
+      <Elements stripe={stripePromise}>
+        <ElementsConsumer>
+          {(elements, stripe) => (
+            <form>
+              <br />
+              <br />
+              <CardElement />
+            </form>
+          )}
+        </ElementsConsumer>
+      </Elements>
     </>
   );
 };
